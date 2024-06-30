b25fafd646696369b84cac3ff5eda70970718f05
Iarina Barabash <yarinabarabash@MacBook-Pro-Arina.local>
2024-06-30 22:52:21 +0400

    + ADD webkitFullscreenEnabled

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -108,7 +108,7 @@ const onTimeUpdate = (event: Event) => {
 }
 
 const onFullscreenChangeHandler = () => {
-  if(!document.fullscreenElement || !document.fullscreenEnabled) {
+  if(!document.fullscreenElement || !document.webkitFullscreenEnabled) {
     isFullScreen.value = false
   }
 }
